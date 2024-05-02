5fda9a5e20d41969f32b0aa1784f682b19713d95
Santiago Quijano <santiquijano77@gmail.com>
2024-05-02 00:20:30 +0000

    Fixed other stuff

--- src/js/component/StarshipsCard.js
+++ src/js/component/StarshipsCard.js
@@ -4,18 +4,18 @@ const StarshipsCard = () => {
   return (
     <div>
       &nbsp;
-      <h1 style={{ color: "red" }}>Starships</h1>
+      <h1 style={{ color: "blue" }}>Starships</h1>
       &nbsp;
       <div className="card">
         <img src="https://placehold.co/400x200" className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Name of the Starship</h5>
-          <ul className="card-text" style={{ listStyle: "none", padding: "0" }}>
+          <h5 className="card-title">Starship</h5>
+          <ul className="card-text" style={{ listStyle: "none", padding: "1" }}>
             <li>Gender</li>
             <li>Hair Color</li>
             <li>Eye Color</li>
           </ul>
-          <a href="#" className="btn btn-primary">Learn more!</a>
+          <a href="#" className="btn btn-primary">Please, learn more!</a>
         </div>
       </div>
     </div>
